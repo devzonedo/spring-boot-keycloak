8cb9be6445c410d95ca612efb1dddfb5ebc6ac0b
Ravinath Fernando <devzonedo@gmail.com>
2022-09-28 18:23:01 +0530

    auth/login with username and password

---
+++ auth-server/src/main/java/com/devz/auth/server/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.devz.auth.server.controller;
+
+import com.devz.auth.server.model.LoginRequest;
+import com.devz.auth.server.model.LoginResponse;
+import com.devz.auth.server.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login (@RequestBody LoginRequest loginRequest){
+      return  loginService.login(loginRequest);
+    }
+}
---
+++ auth-server/src/main/java/com/devz/auth/server/service/LoginService.java
@@ -0,0 +1,43 @@
+package com.devz.auth.server.service;
+
+import com.devz.auth.server.model.LoginRequest;
+import com.devz.auth.server.model.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class LoginService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String issueUrl;
+    @Value("${spring.security.oauth2.client.registration.oauth2-client-credentials.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.oauth2-client-credentials.client-secret}")
+    private String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.oauth2-client-credentials.authorization-grant-type}")
+    private String grantType;
+
+    public ResponseEntity<LoginResponse> login(LoginRequest loginRequest) {
+        HttpHeaders  headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
+        map.add("client_id",clientId);
+        map.add("username",loginRequest.getUsername());
+        map.add("password",loginRequest.getPassword());
+        map.add("grant_type",grantType);
+        map.add("client_secret",clientSecret);
+
+        HttpEntity<MultiValueMap<String,String>> httpEntity = new HttpEntity<>(map,headers);
+        ResponseEntity<LoginResponse> response = restTemplate.postForEntity("http://localhost:8180/auth/realms/auth-realm/protocol/openid-connect/token", httpEntity, LoginResponse.class);
+        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+    }
+}
